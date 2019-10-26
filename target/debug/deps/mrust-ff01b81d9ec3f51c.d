3fbb34707482b6339594a33a09092dadb7fcde5e
minh.vu <minh.vu@tiki.vn>
2019-10-26 10:34:48 +0700

    Hi Rust

---
+++ target/debug/deps/mrust-ff01b81d9ec3f51c.d
@@ -0,0 +1,5 @@
+/Users/lap00584/r-workspace/mrust/target/debug/deps/mrust-ff01b81d9ec3f51c: src/main.rs
+
+/Users/lap00584/r-workspace/mrust/target/debug/deps/mrust-ff01b81d9ec3f51c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/mrust.d
@@ -0,0 +1,1 @@
+/Users/lap00584/r-workspace/mrust/target/debug/mrust: /Users/lap00584/r-workspace/mrust/src/main.rs
